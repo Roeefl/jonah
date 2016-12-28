e552efc314332ea42aa303778c548c686f413247
Roee FL <roeefl@gmail.com>
2016-12-28 15:09:46 +0200

    Basic structure

--- src/main.js
+++ src/main.js
@@ -1,11 +1,52 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
+import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
+
+// import App from './App'
+import Hello from './components/Hello'
+
+// We want to apply VueResource and VueRouter to our Vue instance
+Vue.use(VueResource)
+Vue.use(VueRouter)
 
 /* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  template: '<App/>',
-  components: { App }
+// new Vue({
+//   el: '#app',
+//   template: '<App/>',
+//   components: { App }
+// })
+
+// 1. Define route components.
+// These can be imported from other files
+const Foo = { template: '<div>foo</div>' }
+const Bar = { template: '<div>bar</div>' }
+
+// 2. Define some routes
+// Each route should map to a component. The "component" can
+// either be an actual component constructor created via
+// Vue.extend(), or just a component options object.
+// We'll talk about nested routes later.
+const routes = [
+  { path: '/foo', component: Foo },
+  { path: '/bar', component: Bar },
+  { path: '/hello', component: Hello },
+  { path: '*', component: Hello }
+]
+
+// 3. Create the router instance and pass the `routes` option
+// You can pass in additional options here, but let's
+// keep it simple for now.
+const router = new VueRouter({
+  routes // short for routes: routes
 })
+
+// 4. Create and mount the root instance.
+// Make sure to inject the router with the router option to make the
+// whole app router-aware.
+new Vue({
+  router
+}).$mount('#app')
+
+// Now the app has started!
